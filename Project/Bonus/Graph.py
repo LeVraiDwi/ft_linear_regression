c9eaf5b281966f1c14f88f81a6e8353bfc0da58a
LeVraiDwi <tcosse@student.42.fr>
2025-12-12 16:41:44 +0100

    clean python graph

--- Project/Bonus/Graph.py
+++ Project/Bonus/Graph.py
@@ -1,74 +1,11 @@
 import numpy as np
 import pandas as pd
 import sys
-import math
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 i = 1
-
-# Mean
-def Mean(values: list[float]) -> float:
-    total = 0.0
-    n = len(values)
-    for v in values:
-        total += v
-    mean = total / n if n else float('nan')
-    return mean
-
-# Std
-def Std(values: list[float]) -> float:
-    squared_diff = 0.0
-    n = len(values)
-    mean = Mean(values)
-    for v in values:
-        squared_diff += (v - mean) ** 2
-    std = (squared_diff / n) ** 0.5 if n else float('nan')
-    return std
-
-def Cov(v1: list[float], v2: list[float]) -> list[float]:
-    valueX = np.array(v1)
-    valueY = np.array(v2)
-
-    if (len(valueX) != len(valueY)):
-        raise ValueError("list don't have the same size")
-
-    meanX = Mean(v1)
-    meanY = Mean(v2)
-    
-    cov = np.sum((valueX - meanX) * (valueY - meanY))
-
-    return cov
-
-# Correlation between two set
-def Corr(dataFrame: pd.DataFrame) -> list[float]:
-    features = dataFrame.columns
-
-    if (len(features) != 2):
-        raise ValueError("must have 2 feature")
-    
-    if (features[0] == features[1]):
-        return 1
-    v1 = dataFrame[features[0]]
-    v2 = dataFrame[features[1]]
-    n = len(v1)
-    cov = Cov(v1.values, v2.values)
-    std1 = Std(v1)
-    std2 = Std(v2)
-
-    if std1 == 0 or std2 == 0:
-        return 0
-    return (cov / (std1 * std2)) / n # normalize with n
-
-def meanSQuareError(df):
-    nbCol = len(df.columns)
-    sum = 0
-    for index, row in df.iterrows():
-        sum += row['price'] - row[f"price_{nbCol - 2}"]
-    mse = sum / len(df.index)
-    mse = (mse * mse) ** 0.5
-    return mse
 
 def Update(ln, mileage, thetax, thetay):
     ln.set_data([mileage * thetay + thetax],mileage)
@@ -119,20 +56,20 @@ try:
     print("R²  :", r2)
 
     
-    fig, axes = plt.subplots(nrows=1, ncols=2)
-    axes[0].scatter(dataset_test['price'], dataset_test['km'])
-    ln, = axes[0].plot(df_pred[f"price_{i}"], df_pred['km'])
-    axes[0].set_title(f"Data With Regression")
-    axes[0].set_xlabel("Price")
-    axes[0].set_ylabel("Mileage km")
-    axes[1].scatter(
-            df_final_pred['price'],
-            df_final_pred.iloc[:,-1:],
-            alpha=0.5,
-        )
-    axes[1].set_title(f"correlation between price and predicted price")
-    axes[1].set_xlabel("Price")
-    axes[1].set_ylabel("predicted price")
+    fig, axes = plt.subplots()
+    axes.scatter(dataset_test['price'], dataset_test['km'])
+    ln, = axes.plot(df_pred[f"price_{i}"], df_pred['km'])
+    axes.set_title(f"Data With Regression")
+    axes.set_xlabel("Price")
+    axes.set_ylabel("Mileage km")
+    #axes[1].scatter(
+    #        df_final_pred['price'],
+    #        df_final_pred.iloc[:,-1:],
+    #        alpha=0.5,
+    #    )
+    #axes[1].set_title(f"correlation between price and predicted price")
+    #axes[1].set_xlabel("Price")
+    #axes[1].set_ylabel("predicted price")
     i += 1
 
     def artist():
@@ -140,12 +77,12 @@ try:
 
     def Update(frame):
         global i
-        axes[0].clear()
-        axes[0].scatter(dataset_test['price'], dataset_test['km'])
-        axes[0].plot(df_pred[f"price_{i}"], df_pred['km'])
-        axes[0].set_title(f"Data With Regression")
-        axes[0].set_xlabel("Price")
-        axes[0].set_ylabel("Mileage km")
+        axes.clear()
+        axes.scatter(dataset_test['price'], dataset_test['km'])
+        axes.plot(df_pred[f"price_{i}"], df_pred['km'])
+        axes.set_title(f"Data With Regression")
+        axes.set_xlabel("Price")
+        axes.set_ylabel("Mileage km")
         i += 1
 
     amimation = FuncAnimation(fig, Update, interval=10, init_func=artist, cache_frame_data=False)
